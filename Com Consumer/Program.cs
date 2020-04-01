d5ee02bad03f62f68a4d2569cd0db8d6b81a583a
Kylon92 <54574833+Kylon92@users.noreply.github.com>
2020-04-01 12:11:46 +0200

    Add files via upload

---
+++ Com Consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using ComShared;
+using System;
+
+namespace Com_Consumer
+{
+    class Program
+    {
+        
+        //C# To C# works fine.
+        //However C# Server to VBA Consumer crashes
+        static void Main(string[] args)
+        {
+            Type comType = Type.GetTypeFromProgID("ComServer.SQL");
+            Object comObj = Activator.CreateInstance(comType);
+            ISQL isql = (ISQL)comObj;
+            isql.Crash();
+        }
+    }
+}
